1fd766f0e6aa618e922ee7081372d1745d8523d9
Shatia Earlangga Pratama <100780815+ShatiaEarlangggaPratama@users.noreply.github.com>
2025-11-15 17:49:20 +0700

    Update import statement for render_frame function

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from simulasi1 import render_frame
+from simulasi import render_frame
 
 st.set_page_config(page_title="Simulasi Lensa Cembung", layout="wide")
 st.title("🔍 Simulasi Lensa Cembung – Versi Web (Streamlit)")
@@ -9,3 +9,4 @@ placeholder = st.empty()
 while True:
     img = render_frame()
     placeholder.image(img, channels="RGB")
+
